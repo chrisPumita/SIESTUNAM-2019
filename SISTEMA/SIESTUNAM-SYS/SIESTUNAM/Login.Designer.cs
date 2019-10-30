4b3236da5518b6459de94ee2cca8ade6fe9500d5
jennifer70 <jeniper.666@outlook.com>
2019-10-30 16:02:48 -0600

    Comit
    Lo que hice

--- SISTEMA/SIESTUNAM-SYS/SIESTUNAM/Login.Designer.cs
+++ SISTEMA/SIESTUNAM-SYS/SIESTUNAM/Login.Designer.cs
@@ -58,9 +58,9 @@
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(11, 5);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 18);
+            this.label4.Size = new System.Drawing.Size(94, 18);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Version 1.1";
+            this.label4.Text = "Version 1.1.1";
             // 
             // label1
             // 
