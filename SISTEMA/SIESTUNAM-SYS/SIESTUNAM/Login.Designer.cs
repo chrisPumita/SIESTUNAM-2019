b6eb7cc22c2d49b2324f70b18bb2ef9711074834
Christan RCSG <christian.floppy@gmail.com>
2019-10-30 15:46:21 -0600

    PRUEBA COLABORACION
    Se modifico algo del codigo simple

--- SISTEMA/SIESTUNAM-SYS/SIESTUNAM/Login.Designer.cs
+++ SISTEMA/SIESTUNAM-SYS/SIESTUNAM/Login.Designer.cs
@@ -60,7 +60,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 18);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Version 1.0";
+            this.label4.Text = "Version 1.1";
             // 
             // label1
             // 
